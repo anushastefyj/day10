8638bae846de34bb63d24dac960cf62a38d185ea
anushastefyj <24hr5a3001@mtieat.org>
2024-12-11 10:33:25 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class object
+{
+    int a1=5;
+}
+public class demo
+{
+    public static void main(String args[])
+    {
+        object  r=new object();
+        System.out.println(r.a1);
+    }
+
+}
