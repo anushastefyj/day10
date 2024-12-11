f76f1f52e09fc3e363166e8c225dac0feed37501
anushastefyj <24hr5a3001@mtieat.org>
2024-12-11 10:37:28 +0530

    Add files via upload

---
+++ accessmodifier.java
@@ -0,0 +1,11 @@
+class accessmodifier
+{
+    public static void s()
+    {
+        System.out.println("anusha stefy");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
