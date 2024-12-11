c0caced7ccbe90f3102f462b9506bf552dd1b004
anushastefyj <24hr5a3001@mtieat.org>
2024-12-11 14:27:49 +0530

    Add files via upload

---
+++ Protectnonvoid.java
@@ -0,0 +1,12 @@
+class Protectnonvoid
+{
+    public static String s()
+    {
+        return"anusha";
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
