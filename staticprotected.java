06c9f812d5defacc0279b51df98605004c0d0f99
anushastefyj <24hr5a3001@mtieat.org>
2024-12-11 14:14:49 +0530

    Add files via upload

---
+++ staticprotected.java
@@ -0,0 +1,13 @@
+class staticprotected
+{
+    protected static void s()
+    {
+        System.out.println("anusha stefy");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+
+}
+                                                                          
